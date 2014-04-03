15a0357dd6be88bc3aef27f797b53e6fa7583cba
Arshad <arshad@chummun.com>
2014-04-04 00:58:28 +0400

    Load settings from data attributes

--- js/slick.js
+++ js/slick.js
@@ -1,18 +1,37 @@
 (function($) {
   Drupal.behaviors.slick = {
     attach: function(context, settings) {
-      settings.slick = settings.slick || Drupal.settings.chosen;
+      settings.slick = settings.slick || Drupal.settings.slick;
 
       // Function to prepare all the options for the slick() call.
       var getElementOptions = function(element) {
-        var options = $.extend({}, settings.slick);
+        // var options = $.extend({}, settings.slick);
+        var options = {};
+        var data = $(element).data();
+
+        for (name in data) {
+          var value = data[name];
+
+          // Handle 0 and 1 to bool conversion.
+          if (settings.slick[name].type == 'boolean') {
+            value = (value == 1) ? true : false;
+          }
+
+          // Get proper js settings name from attribute.
+          name = getSettingsNameFromDataAttributeName(name);
+          options[name] = value;
+        }
 
         // Set views-row as slide element
         options.slide = '> div.views-row';
 
-        console.log(options);
-
         return options;
+      }
+
+      // Returns the proper settings name from the data attribute name.
+      // Example 'slidestoshow' will return 'slidesToShow'.
+      var getSettingsNameFromDataAttributeName = function(name) {
+        return settings.slick[name].name;
       }
 
       $('.slick', context).once('slick', function() {
